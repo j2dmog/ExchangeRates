84ca2ddaeda16a6d53e055d91e9bb964cc3a8f87
M J <j2dmog51@gmail.com>
2023-11-19 21:44:18 +0100

    third commit

--- exchange.js
+++ exchange.js
@@ -3,8 +3,21 @@ const input2 = document.querySelector('input[type = text]')
 const USD = document.querySelector('.USD')
 const ZAR = document.querySelector('.ZAR')
 const btn = document.querySelector('.btn')
-const h2 = document.querySelector('h2')
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
 
+const USDcurrency = async () => {
+    const uApi = await fetch('https://api.frankfurter.app/latest?from=USD')
+    const objFormat2 = await uApi.json()
+    return objFormat2.rates.ZAR
+}
+USD.addEventListener('click', async () => {
+    const Ucurrency = await USDcurrency()
+    btn.addEventListener('click', () =>{
+        screen1.textContent = input1.value * Ucurrency
+        input1.value = ''
+    })
+})
 
 const SARand = async () => {
     const api = await fetch('https://api.frankfurter.app/latest?from=ZAR')
@@ -15,20 +28,7 @@ const SARand = async () => {
 ZAR.addEventListener('click', async () => {
     const sRand = await SARand()
     btn.addEventListener('click', () =>{
-        h2.textContent = input2.value * sRand
+        screen2.textContent = input2.value * sRand
         input2.value = ''
-    })
-})
-
-const USDcurrency = async () => {
-    const uApi = await fetch('https://api.frankfurter.app/latest?from=USD')
-    const objFormat2 = await uApi.json()
-    return objFormat2.rates.ZAR
-}
-USD.addEventListener('click', async () => {
-    const Ucurrency = await USDcurrency()
-    btn.addEventListener('click', () =>{
-        h2.textContent = input1.value * Ucurrency
-        input1.value = ''
     })
 })
